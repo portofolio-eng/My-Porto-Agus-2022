36aadda50d833755d3fbc913eaa1a95d50ba9e25
portofolio-eng <119755005+portofolio-eng@users.noreply.github.com>
2022-12-11 21:34:49 +0800

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const humberger =document.querySelector('.humberger input');
-const nav =document.querySelector('nav ul');
-
-humberger.addEventListener('click', function() {
-    nav.classList.toggle('slide');
-});
